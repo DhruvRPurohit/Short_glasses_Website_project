24dcfd3deed5cb79779b57293cb8aeeb53c5007a
Dhruv <dhruvr.purohit>
2025-08-02 16:06:17 +0530

    footer changed

--- client/src/components/shared/Footer.js
+++ client/src/components/shared/Footer.js
@@ -27,56 +27,56 @@ const Footer = () => {
   const year = new Date().getFullYear();
 
   const sitemaps = [
-    {
-      name: "Features",
-      paths: [
-        {
-          name: "Cool stuff",
-          path: "/",
-        },
-        {
-          name: "Random feature",
-          path: "/",
-        },
-        {
-          name: "Team feature",
-          path: "/",
-        },
-        {
-          name: "Stuff for developers",
-          path: "/",
-        },
-        {
-          name: "Another one",
-          path: "/",
-        },
-        {
-          name: "Last time",
-          path: "/",
-        },
-      ],
-    },
-    {
-      name: "Resources",
-      paths: [
-        {
-          name: "Resource",
-          path: "/",
-        },
-        {
-          name: "Resource name",
-          path: "/",
-        },
-        {
-          name: "Another resource",
-          path: "/",
-        },
-        {
-          name: "Final resource",
-          path: "/",
-        },
-      ],
-    },
+    // {
+    //   name: "Features",
+    //   paths: [
+    //     {
+    //       name: "Cool stuff",
+    //       path: "/",
+    //     },
+    //     {
+    //       name: "Random feature",
+    //       path: "/",
+    //     },
+    //     {
+    //       name: "Team feature",
+    //       path: "/",
+    //     },
+    //     {
+    //       name: "Stuff for developers",
+    //       path: "/",
+    //     },
+    //     {
+    //       name: "Another one",
+    //       path: "/",
+    //     },
+    //     {
+    //       name: "Last time",
+    //       path: "/",
+    //     },
+    //   ],
+    // },
+    // {
+    //   name: "Resources",
+    //   paths: [
+    //     {
+    //       name: "Resource",
+    //       path: "/",
+    //     },
+    //     {
+    //       name: "Resource name",
+    //       path: "/",
+    //     },
+    //     {
+    //       name: "Another resource",
+    //       path: "/",
+    //     },
+    //     {
+    //       name: "Final resource",
+    //       path: "/",
+    //     },
+    //   ],
+    // },
     {
       name: "About",
       paths: [
@@ -98,28 +98,28 @@ const Footer = () => {
         },
       ],
     },
-    {
-      name: "Contact",
-      paths: [
-        {
-          name: "Help",
-          path: "/",
-        },
-        {
-          name: "Sales",
-          path: "/",
-        },
-        {
-          name: "Advertise",
-          path: "/",
-        },
-      ],
-    },
+    // {
+    //   name: "Contact",
+    //   paths: [
+    //     {
+    //       name: "Help",
+    //       path: "/",
+    //     },
+    //     {
+    //       name: "Sales",
+    //       path: "/",
+    //     },
+    //     {
+    //       name: "Advertise",
+    //       path: "/",
+    //     },
+    //   ],
+    // },
     {
       name: "Legal",
       paths: [
         {
-          name: "Claim",
+          name: "Shot_Glass",
           path: "/",
         },
         {
@@ -132,21 +132,20 @@ const Footer = () => {
         },
       ],
     },
-    {},
     {
       name: "Stay Connected",
       paths: [
         {
           name: "Facebook",
-          path: "https://www.facebook.com/devhasibulislam/",
+          path: "",
         },
         {
           name: "LinkedIn",
-          path: "https://www.linkedin.com/in/devhasibulislam/",
+          path: "",
         },
         {
           name: "GitHub",
-          path: "https://github.com/devhasibulislam/",
+          path: "https://github.com/DhruvRPurohit",
         },
       ],
     },
@@ -155,20 +154,38 @@ const Footer = () => {
   return (
     <footer className="footer-1 bg-gray-100 py-8 sm:py-12 m-6 p-6 rounded-xl">
       <div className="container mx-auto px-4 flex flex-col gap-y-10">
-        <div className="flex md:flex-row md:flex-wrap md:justify-between flex-col gap-x-4 gap-y-8">
+        {/* <div className="flex md:flex-row md:flex-wrap md:justify-between flex-col gap-x-3 gap-y-8">
           {sitemaps?.map((sitemap, index) => (
             <div key={index} className="flex flex-col gap-y-3">
               <h2 className="text-2xl">{sitemap.name}</h2>
               <div className="flex flex-col gap-y-1.5">
                 {sitemap?.paths?.map((path, index) => (
-                  <Link key={index} href={path?.path} className="text-base">
+                  <Link key={index} href={path?.path} className="text-md">
                     {path?.name}
                   </Link>
                 ))}
               </div>
             </div>
           ))}
-        </div>
+        </div> */}
+       <div className="flex flex-wrap px-4">
+        {sitemaps?.map((i, index) => (
+          <div
+            key={index}
+            className="w-full md:w-1/3 flex flex-col justify-center items-start px-4 py-4"
+          >
+            <h2 className="text-2xl">{i.name}</h2>
+            <div className="flex flex-col gap-y-2">
+              {i?.paths?.map((path, index) => (
+                <Link key={index} href={path?.path} className="text-md">
+                  {path?.name}
+                </Link>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+
         <hr />
         <p className="text-center">&copy; {year} Chattary Ajwan Infotech Pvt. Ltd. All rights reserved.</p>
       </div>
