a3eaccc8a5a747dc559b709f15983f4e833a98fc
Dhruv <dhruvr.purohit>
2025-08-04 16:17:04 +0530

    [fix] changed the favicon icon.

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -6,25 +6,26 @@ import { Toaster } from "react-hot-toast";
 export const metadata = {
   metadataBase: new URL("https://canim-csr.vercel.app"),
   title: "Amit Doshi's Shot Glass Collection CMS",
-  description:
-    "Buy Canim - Shop & eCommerce Next.Js Template by Hasibul Islam. Canim | Shop & eCommerce React Template - a responsive React template. Canim is built with the latest Next.Js 13 App Directory",
+  description:"Amit Doshi's Shot Glass Collection Content Management System",
+  icons: {
+    icon: "/favicon.ico",
+  },
   openGraph: {
-    title: "Canim - Shop & eCommerce React Template",
+    title: "Amit Doshi's Shot Glass Collection Content Management System",
     description:
-      "Buy Canim - Shop & eCommerce Next.Js Template by Hasibul Islam. Canim | Shop & eCommerce React Template - a responsive React template. Canim is built with the latest Next.Js 13 App Directory",
-    url: "https://canim-csr.vercel.app",
+      "Amit Doshi's Shot Glass Collection Content Management System",
+    url: "",
     siteName: "Canim Template",
-    images:
-      "https://github.com/devhasibulislam/canim-ecommerce/blob/master/client/public/og.png?raw=true",
+    images: "",
     locale: "en_US",
     type: "website",
   },
   twitter: {
     card: "summary_large_image",
-    site: "@devhasibulislam",
-    title: "Canim - Shop & eCommerce React Template",
+    site: "@dhruvpurohit",
+    title: "Amit Doshi's Shot Glass Collection Content Management System",
     description:
-      "Buy Canim - Shop & eCommerce Next.Js Template by Hasibul Islam. Canim | Shop & eCommerce React Template - a responsive React template. Canim is built with the latest Next.Js 13 App Directory",
+      "Amit Doshi's Shot Glass Collection Content Management System",
     image:
       "https://github.com/devhasibulislam/canim-ecommerce/blob/master/client/public/og.png?raw=true",
   },
